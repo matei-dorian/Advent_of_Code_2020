a631a3320f2f736a60a7cd0cc69448c60a5b0d66
Matei-Dorian Năstase <75504638+matei-dorian@users.noreply.github.com>
2020-12-19 19:09:34 +0200

    Add files via upload

--- advent2.py
+++ advent2.py
@@ -1,82 +1,39 @@
-with open("Input", 'r') as fp:
-    lines = [line.rstrip() for line in fp.readlines()]
-# task 1
-memory = {}
-masks = []
-curr_mask = None
-curr_value = None
-curr_address = None
+def solve(input, count):
+    round = 1
+    stack = {}
+    previous = {}
 
-for line in lines:
-    k, v = line.split(" = ")
-    if k == "mask":
-        masks.append(v)
-        curr_mask = v
-    else:
-        # get address
-        curr_address = int(k[4:-1])
-        curr_value = int(v)
-
-        bin_value = list(bin(curr_value)[2:].zfill(36))
-        new_value = [0] * 36
-
-        for i, (mask, value) in enumerate(zip(curr_mask, bin_value)):
-            # do nothing if X
-            if mask == "X":
-                new_value[i] = value
-            else:
-                # change value to mask else
-                new_value[i] = mask
-
-        memory[curr_address] = int("".join(new_value), 2)
-print(f"Part one solution: {sum(memory.values())}")
-
-with open("Input", 'r') as fp:
-    lines = [line.rstrip() for line in fp.readlines()]
-memory = {}
-for line in lines:
-    k, v = line.split(" = ")
-    if k == "mask":
-        masks.append(v)
-        curr_mask = v
-    else:
-        # get address
-        curr_address = int(k[4:-1])
-        curr_value = int(v)
-
-        bin_add = list(bin(curr_address)[2:].zfill(36))
-        new_add = ["0"] * 36
-
-        v = len(list(bin(curr_address)[2:]))
-
-        for i, (mask, value) in enumerate(zip(curr_mask, bin_add)):
-            # if mask bit is floating then keep it floating on new address too
-            if mask == "X":
-                new_add[i] = "X"
-            elif mask == "0":
-
-                # change value to mask else
-                new_add[i] = value
-            elif mask == "1":
-                new_add[i] = "1"
-
-        new_add = "".join(new_add)
-
-        # count floatings
-        num_poss = new_add.count("X")
-
-        flucts = []
-        for i in range(2**num_poss):
-            flucts.append( list(bin(i)[2:].zfill(num_poss)))
-
-        for fluct in flucts:
-            i = 0
-            nadd = ""
-            for a in new_add:
-                if a == "X":
-                    nadd+=str(fluct[i])
-                    i+=1
+    while round <= count:
+        if round <= len(input):  # initialize the program and add the starting numbeer and their round to the dictionary
+            num = input[round - 1]
+            stack[num] = round
+        else:  # normal round after initialization
+            if num in previous:  # check if the number occured before already
+                num = stack[num] - previous[num]  # if yes, calculate the difference between the last 2 occurences
+                if num in stack:  # check if the number was spoken before
+                    previous[num] = stack[
+                        num]  # if yes, move the information of the last occurence (number and round) to the dict previous
+                    stack[num] = round  # and add the number and the current round to the dict stack
+                else:  # if it is the first time the number is spoken, only add it to the stack
+                    stack[num] = round
+            else:  # if the number wasnt spoken at least 2 times, set the number to 0
+                num = 0
+                if num in stack:  # check if 0 already exists in the stack
+                    previous[num] = stack[
+                        num]  # if yes, move the information of the last occurence (0 and round) to the dict previous
+                    stack[num] = round  # and add 0 and the current round to the dict stack
                 else:
-                    nadd+=str(a)
-            memory[int(nadd, 2)]=curr_value
-print(sum(memory.values()))
+                    stack[num] = round  # if 0 wasnt spoken before, just add it to the stack
+        round += 1
+    return num
+
+
+f = open("Input")
+f = f.read()
+f = f.split(',')
+l = [int(x) for x in f]
+
+
+
+print("PartI:",solve(l, 2020))
+print("PartII:",solve(l, 30000000))
